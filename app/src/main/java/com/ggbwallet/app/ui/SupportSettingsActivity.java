ba8461e5ecda9e186b38c32467ea071bacfb584b
ggb.best <www.ggb.best@gmail.com>
2021-08-11 11:30:28 +0900

    지원 url변경

--- app/src/main/java/com/ggbwallet/app/ui/SupportSettingsActivity.java
+++ app/src/main/java/com/ggbwallet/app/ui/SupportSettingsActivity.java
@@ -19,10 +19,10 @@ public class SupportSettingsActivity extends BaseActivity {
     private LinearLayout supportSettingsLayout;
 
     private SettingsItemView telegram;
-    private SettingsItemView twitter;
-    private SettingsItemView reddit;
-    private SettingsItemView facebook;
-    private SettingsItemView blog;
+//    private SettingsItemView twitter;
+//    private SettingsItemView reddit;
+//    private SettingsItemView facebook;
+//    private SettingsItemView blog;
     private SettingsItemView faq;
 
     @Override
@@ -45,29 +45,29 @@ public class SupportSettingsActivity extends BaseActivity {
                 .withListener(this::onTelegramClicked)
                 .build();
 
-        twitter = new SettingsItemView.Builder(this)
-                .withIcon(R.drawable.ic_logo_twitter)
-                .withTitle(R.string.twitter)
-                .withListener(this::onTwitterClicked)
-                .build();
-
-        reddit = new SettingsItemView.Builder(this)
-                .withIcon(R.drawable.ic_logo_reddit)
-                .withTitle(R.string.reddit)
-                .withListener(this::onRedditClicked)
-                .build();
-
-        facebook = new SettingsItemView.Builder(this)
-                .withIcon(R.drawable.ic_logo_facebook)
-                .withTitle(R.string.facebook)
-                .withListener(this::onFacebookClicked)
-                .build();
-
-        blog = new SettingsItemView.Builder(this)
-                .withIcon(R.drawable.ic_settings_blog)
-                .withTitle(R.string.title_blog)
-                .withListener(this::onBlogClicked)
-                .build();
+//        twitter = new SettingsItemView.Builder(this)
+//                .withIcon(R.drawable.ic_logo_twitter)
+//                .withTitle(R.string.twitter)
+//                .withListener(this::onTwitterClicked)
+//                .build();
+//
+//        reddit = new SettingsItemView.Builder(this)
+//                .withIcon(R.drawable.ic_logo_reddit)
+//                .withTitle(R.string.reddit)
+//                .withListener(this::onRedditClicked)
+//                .build();
+//
+//        facebook = new SettingsItemView.Builder(this)
+//                .withIcon(R.drawable.ic_logo_facebook)
+//                .withTitle(R.string.facebook)
+//                .withListener(this::onFacebookClicked)
+//                .build();
+//
+//        blog = new SettingsItemView.Builder(this)
+//                .withIcon(R.drawable.ic_settings_blog)
+//                .withTitle(R.string.title_blog)
+//                .withListener(this::onBlogClicked)
+//                .build();
 
         faq = new SettingsItemView.Builder(this)
                 .withIcon(R.drawable.ic_settings_faq)
@@ -81,18 +81,18 @@ public class SupportSettingsActivity extends BaseActivity {
         if (MediaLinks.AWALLET_TELEGRAM_URL != null) {
             supportSettingsLayout.addView(telegram);
         }
-        if (MediaLinks.AWALLET_TWITTER_URL != null) {
-            supportSettingsLayout.addView(twitter);
-        }
-        if (MediaLinks.AWALLET_REDDIT_URL != null) {
-            supportSettingsLayout.addView(reddit);
-        }
-        if (MediaLinks.AWALLET_FACEBOOK_URL != null) {
-            supportSettingsLayout.addView(facebook);
-        }
-        if (MediaLinks.AWALLET_BLOG_URL != null) {
-            supportSettingsLayout.addView(blog);
-        }
+//        if (MediaLinks.AWALLET_TWITTER_URL != null) {
+//            supportSettingsLayout.addView(twitter);
+//        }
+//        if (MediaLinks.AWALLET_REDDIT_URL != null) {
+//            supportSettingsLayout.addView(reddit);
+//        }
+//        if (MediaLinks.AWALLET_FACEBOOK_URL != null) {
+//            supportSettingsLayout.addView(facebook);
+//        }
+//        if (MediaLinks.AWALLET_BLOG_URL != null) {
+//            supportSettingsLayout.addView(blog);
+//        }
         supportSettingsLayout.addView(faq);
     }
 
@@ -110,69 +110,69 @@ public class SupportSettingsActivity extends BaseActivity {
         }
     }
 
-    private void onLinkedInClicked() {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(MediaLinks.AWALLET_LINKEDIN_URL));
-        if (isAppAvailable(C.LINKEDIN_PACKAGE_NAME)) {
-            intent.setPackage(C.LINKEDIN_PACKAGE_NAME);
-        }
-        try {
-            startActivity(intent);
-        } catch (Exception e) {
-            FirebaseCrashlytics.getInstance().recordException(e);
-            e.printStackTrace();
-        }
-    }
-
-    private void onTwitterClicked() {
-        Intent intent;
-        try {
-            getPackageManager().getPackageInfo(C.TWITTER_PACKAGE_NAME, 0);
-            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MediaLinks.AWALLET_TWITTER_URL));
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        } catch (Exception e) {
-            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MediaLinks.AWALLET_TWITTER_URL));
-        }
-        try {
-            startActivity(intent);
-        } catch (Exception e) {
-            FirebaseCrashlytics.getInstance().recordException(e);
-            e.printStackTrace();
-        }
-    }
-
-    private void onRedditClicked() {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        if (isAppAvailable(C.REDDIT_PACKAGE_NAME)) {
-            intent.setPackage(C.REDDIT_PACKAGE_NAME);
-        }
-
-        intent.setData(Uri.parse(MediaLinks.AWALLET_REDDIT_URL));
-
-        try {
-            startActivity(intent);
-        } catch (Exception e) {
-            FirebaseCrashlytics.getInstance().recordException(e);
-            e.printStackTrace();
-        }
-    }
-
-    private void onFacebookClicked() {
-        Intent intent;
-        try {
-            getPackageManager().getPackageInfo(C.FACEBOOK_PACKAGE_NAME, 0);
-            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MediaLinks.AWALLET_FACEBOOK_URL));
-            //intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MediaLinks.AWALLET_FACEBOOK_ID));
-        } catch (Exception e) {
-            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MediaLinks.AWALLET_FACEBOOK_URL));
-        }
-        try {
-            startActivity(intent);
-        } catch (Exception e) {
-            FirebaseCrashlytics.getInstance().recordException(e);
-            e.printStackTrace();
-        }
-    }
+//    private void onLinkedInClicked() {
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.setData(Uri.parse(MediaLinks.AWALLET_LINKEDIN_URL));
+//        if (isAppAvailable(C.LINKEDIN_PACKAGE_NAME)) {
+//            intent.setPackage(C.LINKEDIN_PACKAGE_NAME);
+//        }
+//        try {
+//            startActivity(intent);
+//        } catch (Exception e) {
+//            FirebaseCrashlytics.getInstance().recordException(e);
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private void onTwitterClicked() {
+//        Intent intent;
+//        try {
+//            getPackageManager().getPackageInfo(C.TWITTER_PACKAGE_NAME, 0);
+//            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MediaLinks.AWALLET_TWITTER_URL));
+//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        } catch (Exception e) {
+//            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MediaLinks.AWALLET_TWITTER_URL));
+//        }
+//        try {
+//            startActivity(intent);
+//        } catch (Exception e) {
+//            FirebaseCrashlytics.getInstance().recordException(e);
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private void onRedditClicked() {
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        if (isAppAvailable(C.REDDIT_PACKAGE_NAME)) {
+//            intent.setPackage(C.REDDIT_PACKAGE_NAME);
+//        }
+//
+//        intent.setData(Uri.parse(MediaLinks.AWALLET_REDDIT_URL));
+//
+//        try {
+//            startActivity(intent);
+//        } catch (Exception e) {
+//            FirebaseCrashlytics.getInstance().recordException(e);
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private void onFacebookClicked() {
+//        Intent intent;
+//        try {
+//            getPackageManager().getPackageInfo(C.FACEBOOK_PACKAGE_NAME, 0);
+//            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MediaLinks.AWALLET_FACEBOOK_URL));
+//            //intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MediaLinks.AWALLET_FACEBOOK_ID));
+//        } catch (Exception e) {
+//            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MediaLinks.AWALLET_FACEBOOK_URL));
+//        }
+//        try {
+//            startActivity(intent);
+//        } catch (Exception e) {
+//            FirebaseCrashlytics.getInstance().recordException(e);
+//            e.printStackTrace();
+//        }
+//    }
 
     private void onBlogClicked() {
 
